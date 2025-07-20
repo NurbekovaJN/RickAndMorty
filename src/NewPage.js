40c8f9b820f1514b90a7b88cc2395481e9147e6b
J.N.N <j.nurbekova98@gmail.com>
2025-07-20 17:52:09 +0300

    add new page
    add new page

---
+++ src/NewPage.js
@@ -0,0 +1,12 @@
+import './Main.css'
+
+
+const MainPage = () => {
+    return (
+        <h1 className="MainGreeting">
+            Добро пожаловать в мир "Рик и Морти"
+        </h1>
+    );
+};
+
+export default MainPage;
